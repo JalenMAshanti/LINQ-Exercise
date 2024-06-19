ecb78d5a86e707224eaa65c8d90bec1bfa7e355c
Jalen Ashanti <jalenmshaw@gmail.com>
2024-06-19 11:22:46 -0400

    Completed Linq Exercise1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> list = new List<string>() {"Animal Crossing",  "Legend Of Zelda", "Elden Ring", "Bioshock", "Mario Kart", "Super Smash Bros"};
+
+            Console.WriteLine("Method Syntax----------------------");
+            // With Method Syntax------------------------
+
+            var orderBy = list.OrderBy(x => x.Length);
+            foreach (var game in orderBy)
+            {
+                Console.WriteLine(game);
+            }
+
+            Console.WriteLine("Query Syntax----------------------");
+            //With Query Syntax---------------------------
+
+            var result = from game in list
+                         orderby game.Length
+                         select game;
+
+            foreach (var item in result) 
+            { 
+                Console.WriteLine(item);
+            }
+                         
         }
     }
 }
